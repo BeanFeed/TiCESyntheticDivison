2530e0fc963d2abf753a97beffd9925e05b0783a
BeanFeed <two.austin.two@gmail.com>
2023-01-25 13:53:39 -0500

    fix

--- src/main.c
+++ src/main.c
@@ -8,26 +8,30 @@
 */
 #include <stdlib.h>
 #include <ti/screen.h>
+#include <ti/getcsc.h>
 #include <stdio.h>
 int main(void)
 {
     os_ClrHome();
     os_SetCursorPos(0,0);
     int polyLength;
-    char pL[1];
-    os_GetStringInput("Polynomial Length: ", pL,1);
+    char pL[5];
+    os_GetStringInput("Polynomial Length: ", pL,5);
+    os_SetCursorPos(1,0);
     polyLength = atoi(pL);
     float nums[polyLength];
     for(int i = 0; i < polyLength; i++) {
         char pL[2];
         char prompt[10];
-        sprintf(prompt,"Num%d: ",i);
+        sprintf(prompt,"Num%d: ",i + 1);
         os_GetStringInput(prompt,pL,2);
+        os_SetCursorPos(i + 2,0);
         nums[i] = atof(pL);
     }
     char divStr[2];
     float divNum;
     os_GetStringInput("Dividing Number: ", divStr,2);
+    os_SetCursorPos(polyLength + 2,0);
     divNum = atof(divStr);
     char out[20];
     float outNums[polyLength];
@@ -43,5 +47,6 @@ int main(void)
         sprintf(out,"%s, %f",out, outNums[i]);
     }
     os_PutStrLine(out);
+    while(!os_GetCSC());
     return 0;
 }
